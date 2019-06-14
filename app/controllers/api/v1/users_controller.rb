46b173ac1f2a99c6773d8bfffd5ef60d0fd8d192
github username <github email address>
2019-06-14 16:36:34 -0400

    users can be stored

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
+    byebug
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
