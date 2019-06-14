d88309ded6779898b9f6faaf5b3add968773efdc
github username <github email address>
2019-06-14 17:29:13 -0400

    sends user token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,11 +10,10 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    byebug
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      render json: { user: UserSerializer.new(@user), jwt: @token}, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
