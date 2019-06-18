7d00d8a7bdd66f28728a347d031882ca1fcd0ed0
github username <github email address>
2019-06-18 10:20:45 -0400

    delet works

--- app/models/pad.rb
+++ app/models/pad.rb
@@ -1,5 +1,5 @@
 class Pad < ApplicationRecord
   belongs_to :setup
-  has_many :oscillators
+  has_many :oscillators, dependent: :destroy
   accepts_nested_attributes_for :oscillators
 end
--- app/models/setup.rb
+++ app/models/setup.rb
@@ -1,5 +1,5 @@
 class Setup < ApplicationRecord
   belongs_to :user
-  has_many :pads
+  has_many :pads, dependent: :destroy
   accepts_nested_attributes_for :pads
 end
