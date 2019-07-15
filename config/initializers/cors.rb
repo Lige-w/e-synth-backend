c103cd1aae8a9163fc69ee1656d1ca16f599c033
github username <github email address>
2019-07-15 16:22:22 -0400

    ready for heroku deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ['https://mysynth.herokuapp.com', 'http://mysynth.herokuapp.com']
 
     resource '*',
       headers: :any,
